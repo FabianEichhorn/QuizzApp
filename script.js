8862eb31c776b6d883375ddd3c453cdc558860ee
FabianEichhorn <fabihorn.go@gail.com>
2022-04-04 15:24:35 +0200

    first structures added

---
+++ script.js
@@ -0,0 +1,41 @@
+let questions = [{
+        'questions': 'Wie ist das Grundgerüst einer HTML Datei?',
+        'answer_1': '<HTML><HEAD><BODY></BODY></HEAD></HTML>',
+        'answer_2': '<HTML></HTML> ',
+        'answer_3': 'gibt kein Grundgerüst',
+        'answer_4': '<HTML><HEAD></HEAD><BODY></BODY></HTML>',
+        'right_answer': 4
+    },
+    {
+        'questions': 'Gibt es ein Zeichenlimit für HTML Dateien?',
+        'answer_1': 'ja 150.000 Zeichen',
+        'answer_2': 'Nein',
+        'answer_3': 'Nein, wenn JavaScript miteingebunden ist',
+        'answer_4': 'Nein, wenn man alles in eine große Div packt',
+        'right_answer': 2
+    },
+    {
+        'questions': 'Wie kann man seine HTML-Datei stylen?',
+        'answer_1': 'CSS',
+        'answer_2': 'JavaScript',
+        'answer_3': 'JSONs',
+        'answer_4': 'gar nicht',
+        'right_answer': 1
+    },
+    {
+        'questions': 'Wie kann man Elemente in einer Div zentrieren?',
+        'answer_1': 'mit !center',
+        'answer_2': 'mit einer funktion in JS',
+        'answer_3': 'auf dem Stuhl im Kreis drehen',
+        'answer_4': 'mit display: flex, Justify-content: center, align-items: center',
+        'right_answer': 4
+    },
+    {
+        'questions': 'Wie bindet man eine style.css Datei in eine HTML Datei ein?',
+        'answer_1': 'mit <link rel="stylesheet" href="style.css"> im body bereich',
+        'answer_2': 'geht nur mit pHp',
+        'answer_3': 'mit <link rel="stylesheet" href="style.css"> im head bereich',
+        'answer_4': 'mit !important',
+        'right_answer': 3
+    },
+];
