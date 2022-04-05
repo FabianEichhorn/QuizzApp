0407db51ed026e4c7af39f23be8a21934debef7a
FabianEichhorn <fabihorn.go@gail.com>
2022-04-05 16:07:39 +0200

    addet Endcreen

--- script.js
+++ script.js
@@ -46,18 +46,25 @@ let currentQuestion = 0;
 
 function init() {
     showQuestion();
+    document.getElementById('current-question').innerHTML = currentQuestion + 1
     document.getElementById('all-questions').innerHTML = questions.length; // Anzahl der Fragen die beantwortet werden sollen mit der länge des JSONs angegeben
 }
 
 
 function showQuestion() {
-    let question = questions[currentQuestion];
 
-    document.getElementById('questiontext').innerHTML = question['questions'];
-    document.getElementById('answer_1').innerHTML = question['answer_1'];
-    document.getElementById('answer_2').innerHTML = question['answer_2'];
-    document.getElementById('answer_3').innerHTML = question['answer_3'];
-    document.getElementById('answer_4').innerHTML = question['answer_4'];
+    if (currentQuestion >= questions.length) {
+        document.getElementById('endscreen').style = '';
+        document.getElementById('question-body').style = 'display: none'
+    } else {
+        let question = questions[currentQuestion];
+        //statt bei init und bei next question könnte man die zahl unten auch mit einer Zeile weiterschalten lassen und zwar hier mit: documen.getElementById('current-question').innerHTML = currentQuestion +1
+        document.getElementById('questiontext').innerHTML = question['questions'];
+        document.getElementById('answer_1').innerHTML = question['answer_1'];
+        document.getElementById('answer_2').innerHTML = question['answer_2'];
+        document.getElementById('answer_3').innerHTML = question['answer_3'];
+        document.getElementById('answer_4').innerHTML = question['answer_4'];
+    }
 }
 
 
@@ -68,12 +75,32 @@ function answer(selection) {
     let idOfRightAnswer = `answer_${question['right_answer']}`; // damit gibt man in abhängigkeit der Frage an welche Antwort richtig ist (wichtig welche Antwort dann grün werden muss in der else abfrage)
 
     if (selectedQuestionNumber == question['right_answer']) {
-        console.log('Richtige Antwort');
         document.getElementById(selection).parentNode.classList.add('bg-success'); // warum ist hier (selection)? --> selection ist die Variable für die in der HTML datei stehenden komponenten (zb. bei der ersten Antwort function answer('answer_1'))
     } else {
-        console.log('Falsche Antwort');
         document.getElementById(selection).parentNode.classList.add('bg-danger'); // .parentNode gibt die classList auf das übergeordnete Element weiter. In dem Fall die Div mit der onclick answer() funktion
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
     }
     document.getElementById('nex-button').disabled = false; // button wird wieder enabled wenn man auf ne antwort klickt
+}
+
+
+function nextQuestion() {
+    currentQuestion++; // damit currentQuestion nicht mehr 0 ist sondern 1, 2, 3...
+    document.getElementById('nex-button').disabled = true;
+    resetAnswerButtons();
+    document.getElementById('current-question').innerHTML = currentQuestion + 1 // Frage wird unten links von 1 auf 2, 3, 4, 5 weiter gestellt
+    showQuestion();
+
+}
+
+
+function resetAnswerButtons() {
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
 }
